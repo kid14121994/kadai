e573de31c8baece2ff3f294dc69a3720d2c32671
kid14121994 <79203950+kid14121994@users.noreply.github.com>
2021-04-13 15:53:24 +0900

    js party!

--- coding2/js/script.js
+++ coding2/js/script.js
@@ -1,10 +1,12 @@
 $(function () {
-    $('#headder_toggle').click(function () {
+    $('#header_toggle').click(function () {
         //menu非表示処理/////
-        if($('.line').hasClass('rotate')){
-            $('.line').removeClass('rotate');
+        if($('#line').hasClass('rotate')){
+            $('#line').removeClass('rotate');
+            $('.sp-nav').hide(1000);
         }else{//menu表示処理/////
-            $('.line').addClass('rotate');
+            $('#line').addClass('rotate');
+            $('.sp-nav').show(1000);
         }
     });
 });
